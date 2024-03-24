622d30dfa5aa3f95b3f22a0585052ec8b7e4f239
Ivan Zhou <ivanz@stanford.edu>
2024-03-24 16:34:53 +0000

    Add Llama2 7B pretrain
    update per device parallelism
    
    queued-resource
    
    add --project "hai-gcp-models"
    
    update train config
    
    increase seq len
    
    reduce batch size to 2k
    
    1T tokens
    
    layer_norm_epsilon: float = 1e-6

--- infra/spin-up-vm.sh
+++ infra/spin-up-vm.sh
@@ -12,8 +12,10 @@ fi
 
 # Determine the correct gcloud command based on USE_ALPHA flag
 GCLOUD_CMD="gcloud compute"
+GCLOUD_ACTION="tpu-vm"
 if [ "$USE_ALPHA" = true ]; then
   GCLOUD_CMD="gcloud alpha compute"
+  GCLOUD_ACTION="queued-resources"
 fi
 
 # first delete if we're supposed to
@@ -36,15 +38,13 @@ fi
 # spin loop until we get a good error code
 echo "Creating VM $VM_NAME"
 # create the command. note that --preemptible doesn't accept a value, so just append it if we want it
-CMD="$GCLOUD_CMD tpus tpu-vm create $VM_NAME \
+CMD="$GCLOUD_CMD tpus $GCLOUD_ACTION create $VM_NAME \
+  --node-id=$VM_NAME \
   --zone=$ZONE \
   --accelerator-type=$TYPE \
-  --version=$VM_IMAGE"
-
-# if network isn't 'default', set it
-if [ "$NETWORK" != "default" ]; then
-  CMD="$CMD --network=$NETWORK"
-fi
+  --runtime-version=$VM_IMAGE \
+  --subnetwork=$SUBNETWORK \
+  --reserved"
 
 if [ "$PREEMPTIBLE" = true ]; then
   CMD="$CMD --preemptible"
@@ -68,9 +68,9 @@ SETUP_SCRIPT_NAME=$(basename $SETUP_SCRIPT)
 # note that gcloud scp doesn't always work... so we do it a few times to just be sure
 for i in {1..5}; do
   echo "Uploading $SETUP_SCRIPT to VM $VM_NAME"
-  $GCLOUD_CMD tpus tpu-vm scp --zone=$ZONE $SETUP_SCRIPT $VM_NAME:~/ --worker=all
+  $GCLOUD_CMD tpus tpu-vm scp --zone=$ZONE --project "hai-gcp-models" $SETUP_SCRIPT $VM_NAME:~/ --worker=all
   # check to see if the file exists on all nodes
-  if $GCLOUD_CMD tpus tpu-vm ssh --zone=$ZONE $VM_NAME --command="ls ~/$SETUP_SCRIPT_NAME" --worker=all; then
+  if $GCLOUD_CMD tpus tpu-vm ssh --zone=$ZONE --project "hai-gcp-models" $VM_NAME --command="ls ~/$SETUP_SCRIPT_NAME" --worker=all; then
     break
   fi
   if [ 5 -eq $i ]; then
@@ -83,7 +83,7 @@ done
 
 # run the setup script
 for i in {1..5}; do
-  $GCLOUD_CMD tpus tpu-vm ssh --zone=$ZONE $VM_NAME --command="bash ~/$SETUP_SCRIPT_NAME --branch ${GIT_BRANCH} --repo ${GIT_REPO} > setup.out" --worker=all
+  $GCLOUD_CMD tpus tpu-vm ssh --zone=$ZONE $VM_NAME --project "hai-gcp-models" --command="bash ~/$SETUP_SCRIPT_NAME --branch ${GIT_BRANCH} --repo ${GIT_REPO} > setup.out" --worker=all
   if [ $? -eq 0 ]; then
     break
   fi
